d6be5a2ae0292f6585149edc50acda6c5b1f3764
Chris Atkinson <catkinson@gmail.com>
2015-12-26 14:44:11 +0000

    rejig

--- puppet/modules/brumulus/manifest/init.pp
+++ puppet/modules/brumulus/manifest/init.pp
@@ -8,26 +8,3 @@ class brumulus {
   # class { '::brumulus::service': }
 
 }
-
-
-
-class brumulus::service{
-
-supervisor::program { $SERVICE:
- ensure      => present,
- enable      => true,
- command     => '/usr/bin/python Brumulus.py',
- directory   => "${APP_HOME}/py/control/",
- user        => $USER,
- group       => $USER,
- logdir_mode => '0770',
-}
-
-# service { $SERVICE:
-#    ensure  => running,
-#    enable  => true,
-#  } 
-
-}
-
-}
