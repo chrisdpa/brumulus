a1be048d7789fa6d22f72c7621f107ed43469cb1
Chris Atkinson <catkinson@gmail.com>
2018-11-04 18:00:26 +0000

    almost

--- brumulus/bru.py
+++ brumulus/bru.py
@@ -2,14 +2,14 @@ import requests
 import time
 
 control_endpoint = 'http://192.168.0.35:5002/control/?current_temp={}&target_temp={}&delta_temp_c={}&delta_time_ms={}'
-heater_endpoint = 'http://192.168.0.35:5000/output/0/{}'
-temperature_endpoing = 'http://192.168.0.32:5000/temp'
-temp = requests.get('http://192.168.0.32:5000/temp').content
+heater_endpoint = 'http://192.168.0.35:5003/output/0/{}'
+temperature_endpoint = 'http://192.168.0.32:5000/temp'
+temp = requests.get(temperature_endpoint).content
 prev = temp
 setpoint = 20.0
 
 while (True):
-    temp = requests.get(temperature_endpoing).content
+    temp = requests.get(temperature_endpoint).content
     diff = float(temp) - float(prev)
     control = requests.get(control_endpoint.format(temp, setpoint, diff, 15000)).content
     prev = temp
