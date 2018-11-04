193123b030b22ea5c31c10137004707cd69bdca7
Chris Atkinson <catkinson@gmail.com>
2018-11-04 16:08:34 +0000

    again

--- brumulus/OutputService.py
+++ brumulus/OutputService.py
@@ -6,7 +6,7 @@ import ProtectedOutput as po
 
 app = Flask(__name__)
 
-outputs = [po.ProtectedOutput(so.EnerginieOutput(), min_state_time=10)]
+outputs = [po.ProtectedOutput(so.EnergenieOutput(), min_state_time=10)]
 
 
 @app.route("/output/<int:index>/<string:state>", methods=['GET', 'POST'])
