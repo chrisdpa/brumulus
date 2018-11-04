5b4156efe07a5021cb04536219be54b3c0ef6947
Chris Atkinson <catkinson@gmail.com>
2018-11-04 17:56:48 +0000

    control loop

---
+++ brumulus/bru.py
@@ -0,0 +1,25 @@
+import requests
+import time
+
+control_endpoint = 'http://192.168.0.35:5002/control/?current_temp={}&target_temp={}&delta_temp_c={}&delta_time_ms={}'
+heater_endpoint = 'http://192.168.0.35/output/0/{}'
+temperature_endpoing = 'http://192.168.0.32:5000/temp'
+temp = requests.get('http://192.168.0.32:5000/temp').content
+prev = temp
+setpoint 20
+
+while (True):
+    temp = requests.get(temperature_endpoing).content
+    diff = float(temp) - float(prev)
+    control = requests.get(control_endpoint.format(temp, setpoint, diff, 15000)).content
+    prev = temp
+    if ('Output.off' == control){
+        requests.get(heater_endpoint.format('OFF'))
+    }
+    if ('Output.chill' == control){
+        requests.get(heater_endpoint.format('OFF'))
+    }
+    if ('Output.heat' == control){
+        requests.get(heater_endpoint.format('ON'))
+    }
+    time.sleep(15)
